072d255ee07ddcce2fa2557043aa7dcbde688a39
Johnny Huynh <huynhtuandat.ueh@gmail.com>
2020-09-19 04:18:53 +1000

    added https callable function to request a new course and update firestore

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+const requestModal = document.querySelector(".new-request");
+const requestLink = document.querySelector(".add-request");
+const requestForm = document.querySelector(".new-request form");
+
+requestLink.addEventListener("click", () => {
+  requestModal.classList.add("open");
+});
+
+requestModal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("new-request")) {
+    requestModal.classList.remove("open");
+  }
+});
+
+// add a new request
+requestForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const addRequest = firebase.functions().httpsCallable("addRequest");
+  addRequest({ text: requestForm.request.value })
+    .then(() => {
+      requestForm.reset();
+      requestModal.classList.remove("open");
+      requestForm.querySelector(".error").textContent = "";
+    })
+    .catch((err) => {
+      requestForm.querySelector(".error").textContent = err.message;
+    });
+});
